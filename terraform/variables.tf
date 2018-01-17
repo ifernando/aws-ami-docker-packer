b86e72f26149349f32e2455ec0b8157af4aa111c
Fernando, Ishara <ishara.fernando@pearson.com>
2018-01-17 07:27:49 +0530

    Added VPC configurations and tags along with updating variables in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,3 +5,18 @@ variable "aws_key_name" {
 variable "public_key_path" {
   default = "~/.ssh/id_rsa.pub"
 }
+
+variable "vpc_cidr" {
+  description = "CIDR for the whole VPC"
+  default = "10.0.0.0/16"
+}
+
+variable "owner" {
+  description = "Infrastructure Owner"
+  default = "Ishara Fernando"
+}
+
+variable "environment" {
+  description = "test-docker-ce Environment"
+  default = "test-docker-ce"
+}

8903d65a91bf0d208838cf4dd367e6c1a54c0e8d
Fernando, Ishara <ishara.fernando@pearson.com>
2018-01-17 08:03:48 +0530

    added variables for Ubuntu 16.04 AMI for us-east-1 regions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,3 +25,15 @@ variable "test-docker-ce_subnet_cidr" {
   description = "CIDR for the Docker CE Test Public Subnet"
   default = "10.0.5.0/24"
 }
+
+variable "aws_amis_base" {
+  description = "aws-us-east-1 Ubuntu 16.04 LTS w/ Docker version 17.09.0-ce"
+  default = {
+    us-east-1 = "ami-41e0b93b"
+  }
+}
+
+variable "aws_region" {
+  description = "Default AWS Region"
+  default = "us-east-1"
+}
